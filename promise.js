69dd678fd7015f868875dcdeee86349ebc02bcee
programmermoeda <programmermoeda@gmail.com>
2022-06-17 23:20:04 +0700

    janji

--- promise.js
+++ promise.js
@@ -1,7 +1,38 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (hasil) => {
+  return new Promise((resolve,reject) => {
+    let data = 0;
+    promiseTheaterIXX()
+      .then((reaksi) => {
+        reaksi.forEach((item) => {
+          
+          if (item.hasil === null) {
+            reject("janji tinggal janji")
+          }
+          if (item.hasil === hasil) {
+            data += 1;
+          }
+        });
+      })
+      .then(
+        promiseTheaterVGC()
+        .then((reaksi) => {
+          reaksi.forEach((item) => {
+            
+            if (item.hasil === null) {
+              reject("janji tinggal janji")
+            }
+            if (item.hasil === hasil) {
+              data += 1;
+            }
+          });
+          resolve(data);
+        })
+      );
+  });
+};
 
 module.exports = {
   promiseOutput,
